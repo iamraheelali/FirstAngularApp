11e8a95b4a54587e9a6f8d1cbde383d6ed86e41a
iamraheelali <rahil.alee@gmail.com>
2022-06-22 00:19:03 +0400

    yes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FirstAngularApp01';
+  title = 'My First Angular Js App';
 }
